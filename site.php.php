dd7b01637151fab464a4211bf61e63866b77a146
Cynthiaumutesi202 <cynthiaumutesi88@gmail.com>
2025-04-13 10:00:31 +0200

    Add files via upload

---
+++ site.php.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+     <head> 
+          <meta charset="utf-8">
+
+          <title>
+</title>
+</head>
+<body>
+
+     <?php
+        $characterName="john";
+        $characterAge= 21;
+        echo "<h1> Our First PHP</h1>";
+        echo "<hr>";
+        echo "<p> This is second line Paragaraphy</p>";
+        echo "there is our first code in php<br>";
+        echo "would like to host our web page<br>";
+        echo "big data is necessary<br>"
+        ?>
+     </body>
+</html>
